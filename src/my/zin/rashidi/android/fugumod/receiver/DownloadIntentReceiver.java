b393a19dfa76a4a42d8137c269cd11a7e3af0ead
shidi <rashidi.zin@onapp.com>
2012-08-22 08:32:09 +0800

    Add IntentReceiver for completed downloaded files.

---
+++ src/my/zin/rashidi/android/fugumod/receiver/DownloadIntentReceiver.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package my.zin.rashidi.android.fugumod.receiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * @author shidi
+ * @version 1.1.0
+ * @since 1.1.0
+ */
+public class DownloadIntentReceiver extends BroadcastReceiver {
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		
+		// Get the checksum
+	}
+
+}
